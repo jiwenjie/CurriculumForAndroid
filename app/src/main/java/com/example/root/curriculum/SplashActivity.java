38f4dd4a91c4ee4eed0ddbf1709b2b2e829642d4
Jack <278630464@qq.com>
2018-06-28 22:50:57 +0800

    修复了一些bug，增加了启动界面
    Signed-off-by: Jack <278630464@qq.com>

--- app/src/main/java/com/example/root/curriculum/SplashActivity.java
+++ app/src/main/java/com/example/root/curriculum/SplashActivity.java
@@ -1,11 +1,36 @@
 package com.example.root.curriculum;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.root.curriculum.activity.MainActivity;
 
 /**
  * App的启动界面
  */
 public class SplashActivity extends AppCompatActivity {
 
+    private Handler handler;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler = new Handler();
+
+        //延迟 2 秒后进入主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        },2000);
+
+    }
 }
